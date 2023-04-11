5eced3af9c4dd40b1de9321ab5345e26e2592852
Vasant Saraswat <vasant.saraswat@gmail.com>
2023-04-11 14:41:20 +0530

    Project started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import './styling/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h1 className='text-5xl text-gray-900'>Hello World</h1>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
